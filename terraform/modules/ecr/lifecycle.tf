bc697f4850aaf18fcce45e767f2d153b318a0af0
Akash <akasham2105@gmail.com>
2026-07-23 01:30:20 +0530

    feat(terraform): implement reusable Amazon ECR module

---
+++ terraform/modules/ecr/lifecycle.tf
@@ -0,0 +1,25 @@
+resource "aws_ecr_lifecycle_policy" "policy" {
+
+  for_each = aws_ecr_repository.repositories
+
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep latest 20 images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 20
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
