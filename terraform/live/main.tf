b52cfc774ec4fe97c450e9b63f110806cb8028ef
Akash <akasham2105@gmail.com>
2026-07-22 14:05:07 +0530

    feat(terraform): add bootstrap and initialize VPC infrastructure

---
+++ terraform/live/main.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+
+  source = "../../modules/vpc"
+
+  project_name = local.project_name
+
+  environment = var.environment
+
+  vpc_cidr = "10.0.0.0/16"
+
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+
+  public_subnet_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnet_cidrs = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
+
+}
